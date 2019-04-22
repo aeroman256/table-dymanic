0dfe8af50df8e2a83e32440039ab4e35b45698c0
aeroman256 <zakharov.dmitry.92@yandex.ru>
2019-04-22 14:50:39 +0300

    add Header component with App, add Search field

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import Header from "./components/Header"
 // import Filter from "./components/Filter"
-import Table from "./components/Table"
+import CustomPaginationActionsTable from "./components/Table"
 // import UserInfo from "./components/UserInfo"
 // import Pagination from "./components/TablePagination"
 
@@ -9,18 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Table />
-          {/* <Header />
-          <Filter />
-          <Table />
-          <UserInfo />
-          <Pagination /> */}
+          <CustomPaginationActionsTable />
       </div>
     );
   }
 }
-//header
-      //FilterableUsersTable -> filter
-      //                  L  -> table -> tableRows -> tableRowsInfo
-      //                  L  -> pagination
+
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,69 @@
-import React from "react"
+import React from 'react';
+import PropTypes from 'prop-types';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const Header = () => (
-    <div>
-        <div className="header">
-            <div className="header__content">
-                <h4>{"Tabular app"}</h4>
-            </div>
-        </div>
-    </div>
-);
 
-export default Header
+const rows = [
+    { id: 'id', numeric: false, disablePadding: false, label: 'id' },
+    { id: 'firstName', numeric: false, disablePadding: false, label: 'First Name' },
+    { id: 'lastName', numeric: false, disablePadding: false, label: 'Last Name' },
+    { id: 'email', numeric: false, disablePadding: false, label: 'email' },
+    { id: 'phone', numeric: false, disablePadding: false, label: 'Phone' },
+  ];
+
+class EnhancedTableHead extends React.Component {
+    createSortHandler = property => event => {
+      this.props.onRequestSort(event, property);
+    };
+  
+    render() {
+      const { order, orderBy } = this.props;
+  
+      return (
+        <TableHead>
+          <TableRow>
+            {rows.map(
+              row => (
+                <TableCell
+                  key={row.id}
+                  align={row.numeric ? 'left' : 'right'}
+                  padding={row.disablePadding ? 'none' : 'default'}
+                  sortDirection={orderBy === row.id ? order : false}
+                >
+                  <Tooltip
+                    title="Sort"
+                    placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                    enterDelay={300}
+                  >
+                    <TableSortLabel
+                      active={orderBy === row.id}
+                      direction={order}
+                      onClick={this.createSortHandler(row.id)}
+                    >
+                      {row.label}
+                    </TableSortLabel>
+                  </Tooltip>
+                </TableCell>
+              ),
+              this,
+            )}
+          </TableRow>
+        </TableHead>
+      );
+    }
+  }
+  
+  EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.string.isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+  };
+  
+  export default EnhancedTableHead;
