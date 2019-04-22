fe77b5c8d97686ad4b0d241fcec291ef45e9fe6e
aeroman256 <zakharov.dmitry.92@yandex.ru>
2019-04-22 14:52:54 +0300

    add search field

---
+++ src/components/Search.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+class SearchTextFields extends React.Component {
+  state = {
+    name: 'Cat in the Hat',
+    age: '',
+    multiline: 'Controlled',
+    currency: 'EUR',
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="outlined-search"
+          label="Search user"
+          type="search"
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
+      </form>
+    );
+  }
+}
+
+SearchTextFields.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SearchTextFields);
