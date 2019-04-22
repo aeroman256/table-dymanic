bef644fea3711bbd102a6524f4aee39d10471771
aeroman256 <zakharov.dmitry.92@yandex.ru>
2019-04-22 17:05:51 +0300

    add handle function for search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   container: {
@@ -18,9 +19,21 @@ const styles = theme => ({
   menu: {
     width: 200,
   },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
 });
 
 class SearchTextFields extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      query: props.query
+    }
+  }
   state = {
     name: 'Cat in the Hat',
     age: '',
@@ -28,9 +41,25 @@ class SearchTextFields extends React.Component {
     currency: 'EUR',
   };
 
-  handleChange = name => event => {
+  // handleChange = name => event => {
+  //   this.setState({
+  //     [name]: event.target.value,
+  //   });
+  // };
+
+  handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      this.handleSearch()
+    }
+  };
+
+  handleSearch = () => {
+    this.props.onQueryChange(this.state.query);
+  };
+
+  handleInputChange = (e) => {
     this.setState({
-      [name]: event.target.value,
+      query: e.target.value,
     });
   };
 
@@ -46,7 +75,18 @@ class SearchTextFields extends React.Component {
           className={classes.textField}
           margin="normal"
           variant="outlined"
+          onKeyDown={this.handleKeyDown}
+          onChange={this.handleInputChange}
+          value={this.state.query}
         />
+        <Button 
+          variant="outlined" 
+          className={classes.button}
+          //onClick={this.handleSearch}
+          onKeyDown={this.handleKeyDown}
+          >
+            <span><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>								</span>
+        </Button>
       </form>
     );
   }
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,17 +2,14 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-// import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-// import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-// import Tooltip from '@material-ui/core/Tooltip';
 import { lighten } from '@material-ui/core/styles/colorManipulator';
 import EnhancedTableHead from './Header'
 import SearchTextFields from './Search'
